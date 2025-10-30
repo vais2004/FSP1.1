55326ada8e5fc1706d379fa1cf64ec542dbeaaa7
vais2004 <vkawale2004@gmail.com>
2025-10-30 18:55:16 +0530

    update feat:  get api /admin/api/data and verifying the token

--- index.js
+++ index.js
@@ -1,18 +1,42 @@
 const express = require("express");
-
+const jwt = require("jsonwebtoken");
 const app = express();
 
 const SECRET_KEY = "supersecretadmin";
+const JWT_SECRET = "your_jwt_secret";
+
 app.use(express.json());
+
+const verifyJWT = (req, res, next) => {
+  const token = req.headers["authorization"];
+
+  if (!token) {
+    return res.status(401).json({ message: "No token provided." });
+  }
+
+  try {
+    //console.log(token);
+    const decodedToken = jwt.verify(token, JWT_SECRET);
+    req.user = decodedToken;
+    next();
+  } catch (error) {
+    return res.status(402).json({ message: "Invalid token" });
+  }
+};
 
 app.post("/admin/login", (req, res) => {
   const { secret } = req.body;
 
   if (secret === SECRET_KEY) {
-    res.json({ message: "Access Granted" });
+    const token = jwt.sign({ role: "admin" }, JWT_SECRET, { expiresIn: "24h" });
+    res.json({ token });
   } else {
     res.json({ message: "Invalid Secret" });
   }
+});
+
+app.get("/admin/api/data", verifyJWT, (req, res) => {
+  res.json({ message: "Protected route accessible" });
 });
 
 app.listen(3000, () => console.log("Server is running on PORT 3000"));
